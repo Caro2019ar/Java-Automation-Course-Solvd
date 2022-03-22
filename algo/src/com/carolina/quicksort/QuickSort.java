be23d1400aeb1844e5eae8e65b12b8de0c95427b
Carolina <argente2019@gmail.com>
2022-03-22 08:19:59 -0300

    Refactor args

--- algo/src/com/carolina/quicksort/QuickSort.java
+++ algo/src/com/carolina/quicksort/QuickSort.java
@@ -2,44 +2,43 @@ package com.carolina.quicksort;
 
 import com.carolina.model.Grade;
 
+import java.util.Arrays;
+
 public class QuickSort {
 
-    public static void initial(Grade[] grades) {
-        sort(grades, 0, grades.length);
-        for (int actual = 0; actual < grades.length; actual++) {
-            Grade grade = grades[actual];
-            System.out.println(grade.getName() + " " + grade.getValue());
-        }
+    public static void initial(Grade[] args) {
+        sort(args, 0, args.length);
+        Arrays.stream(args).map(grade -> grade.getName() + " " + grade.getValue()).forEach(System.out::println);
     }
 
-    private static void sort(Grade[] args, int from, int until) {
+    private static void sort(Grade[] grades, int from, int until) {
         int qtde = until - from;
         if (qtde > 1) {
-            int pivotPosition = particiona(args, from, until);
-            sort(args, from, pivotPosition);
-            sort(args, pivotPosition + 1, until);
+            int pivotPosition = particiona(grades, from, until);
+            sort(grades, from, pivotPosition);
+            sort(grades, pivotPosition + 1, until);
         }
     }
 
-    private static int particiona(Grade[] args, int initial, int end) {
+    private static int particiona(Grade[] grades, int initial, int end) {
         int lowerFound = 0;
-        Grade pivo = args[end - 1];
+        Grade pivo = grades[end - 1];
 
         for (int analyzing = 0; analyzing < end - 1; analyzing++) {
-            Grade actual = args[analyzing];
+            Grade actual = grades[analyzing];
             if (actual.getValue() <= pivo.getValue()) {
-                swap(args, analyzing, lowerFound);
+                swap(grades, analyzing, lowerFound);
                 lowerFound++;
             }
         }
-        swap(args, end - 1, lowerFound);
+        swap(grades, end - 1, lowerFound);
         return lowerFound;
     }
 
-    private static void swap(Grade[] args, int from, int to) {
-        Grade grade1 = args[from];
-        Grade grade2 = args[to];
-        args[to] = grade1;
-        args[from] = grade2;
+    private static void swap(Grade[] grades, int from, int to) {
+        Grade grade1 = grades[from];
+        Grade grade2 = grades[to];
+        grades[to] = grade1;
+        grades[from] = grade2;
     }
 }
