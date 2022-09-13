11b7e8850fa88648e694d50f7f1b951433c1ae6d
CAROLINA T MORIYA <argente2019@gmail.com>
2022-09-13 13:48:13 -0300

    database eer and update stmts

--- README.md
+++ README.md
@@ -2,4 +2,7 @@
 
 ### DDL and DML
 
-* Statements examples
+* Database for on-demand project - Teacher's platform to organize classes, files, evaluations and payments from their students.
+
+
+* <img src="eer-teachdb.png">
--- statements/statements.sql
+++ statements/statements.sql
@@ -2,41 +2,50 @@
 
 --### Insertion
 * 10 statements for insertion
+INSERT INTO teachdb.Users (name, phone, email, address_id, birthday) VALUES ("Paulo da Silva", "+55 11 2356-8977","paulo@gmail.com",2, '1980-12-01');
+INSERT INTO teachdb.Users (name, phone, email, address_id, birthday) VALUES ("Hernan Lopez", "+54 15 13 8999-8977","hernanzito@gmail.com",1, '1990-02-04 00:00:00');
+INSERT INTO teachdb.Courses (description, version, instruments_id) VALUES ("Beginner´s course of Jazz", "v1.1",1);
+INSERT INTO teachdb.Courses (description, version, instruments_id) VALUES ("Beginner´s course of Flamenco", "v1.1",2);
+INSERT INTO teachdb.Courses (description, version, instruments_id) VALUES ("Intermediary´s course of Flamenco", "v3.1",2);
+INSERT INTO teachdb.Instruments (name) VALUES ("Electric Guitar");
+INSERT INTO teachdb.Instruments (name) VALUES ("Guitar");
+INSERT INTO teachdb.Classes (description, date, remote_present_id) VALUES("Main classical chords for rhythm", '2022-08-01 10:30:00', 1);
+INSERT INTO teachdb.Classes (description, date, remote_present_id) VALUES("Theory of medieval music", '2022-09-11 12:00:00', 2);
+INSERT INTO teachdb.Classes (description, date, remote_present_id) VALUES("Training lesson for faster playing", '2022-09-12 16:30:00', 1);
 
-INSERT INTO sakila.city (city_id, city, country_id, last_update) VALUES (601, 'Buenos Aires', 6, '2022-09-08 11:12:25');
-INSERT INTO sakila.city (city_id, city, country_id, last_update) VALUES (602, 'Pilar', 6, '2022-09-08 11:19:00');
-INSERT INTO sakila.city (city_id, city, country_id, last_update) VALUES (603, 'Gualeguay', 6, '2022-09-08 11:20:00');
-INSERT INTO sakila.category (category_id, name, last_update) values (17, 'Food', '2022-09-08 11:25:27');
-INSERT INTO sakila.category (category_id, name, last_update) values (18, 'Animal', '2022-09-08 11:26:27');
-INSERT INTO sakila.category (category_id, name, last_update) values (19, '80s', '2022-09-08 11:27:00');
-INSERT INTO sakila.actor (actor_id, first_name, last_name, last_update) VALUES (201, 'ANGELINA', 'JOLIE', '2022-09-08 00:00:33');
-INSERT INTO sakila.actor (actor_id, first_name, last_name, last_update) VALUES (202, 'WILL', 'SMITH', '2022-09-08 11:05:20');
-INSERT INTO sakila.actor (actor_id, first_name, last_name, last_update) VALUES (203, 'TOM', 'CRUISE', '2022-09-08 00:05:00');
-INSERT INTO sakila.actor (actor_id, first_name, last_name, last_update) VALUES (204, 'WINONA', 'RIDER', '2022-09-08 00:07:02');
 
 --### Updating
 
 * 10 statements for updating
+UPDATE `teachdb`.`Users` SET `first_name` = 'Paulo', `last_name` = 'Silva' WHERE (`id` = '1');
+UPDATE `teachdb`.`Users` SET `first_name` = 'Hernan', `last_name` = 'Lopez' WHERE (`id` = '2');
+UPDATE `teachdb`.`Users` SET `first_name` = 'Renan Maria', `last_name` = 'de Jesus' WHERE (`id` = '3');
+update teachdb.Cities set state_id=1 where (id='1');
+update teachdb.Cities set state_id=2 where (id='2');
 
-  UPDATE sakila.customer SET last_name = 'JOHN' WHERE customer_id=2;
-  UPDATE sakila.customer SET email = 'MARY.OHN@gmail.com' WHERE customer_id=592;
-  UPDATE sakila.customer SET active = 0 WHERE first_name = 'AARON';
-  UPDATE sakila.address SET postal_code = 16293 WHERE address_id=539;
-  UPDATE sakila.address SET address = '1817 Livorno Way' WHERE address_id=488;
-  UPDATE sakila.address SET last_update = '2022-09-25 22:32:09' WHERE address_id=391;
-  UPDATE sakila.film SET rental_rate = 4.80 WHERE film_id=789;
-  UPDATE sakila.film SET last_update = '2022-09-15 05:03:42' WHERE film_id=4;
-  UPDATE sakila.film SET description = 'A Fateful Reflection of a Waitress And a Boat' WHERE film_id=998;
-  UPDATE sakila.film SET release_year = 2007 WHERE film_id=882;
 
 --### Deletion
 * 10 statements for deletions.
 
 * 5 alter table.
+ALTER TABLE teachdb.Addresses rename column idAddress to id;
+ALTER TABLE teachdb.Users MODIFY birthday date;
+ALTER TABLE teachdb.Users MODIFY phone VARCHAR(20);
+ALTER TABLE teachdb.Users rename column birthday to date_of_birth;
+ALTER TABLE teachdb.Users ADD column last_name VARCHAR(20);
 
 * 1 big statement to join all tables in the database.
 
 * 5 statements with left, right, inner, outer joins.
+SELECT * FROM Addresses a LEFT JOIN Cities c on a.city_id = c.id;
+SELECT * FROM States s RIGHT JOIN Countries co on s.country_id = co.id;
+SELECT * FROM States s INNER JOIN Countries co on s.country_id = co.id;
+SELECT * FROM Addresses a LEFT JOIN Cities c on a.city_id = c.name; -- OUTER JOIN
+SELECT u.*, ci.name as 'state', co.name as 'country' FROM Users u INNER JOIN Addresses a on a.id = u.address_id
+INNER JOIN Cities ci on a.city_id = ci.id
+INNER JOIN States st on st.idState = ci.state_id
+INNER JOIN Countries co on co.id = st.country_id
+where co.name = 'Brazil';
 
 * 7 statements with aggregate functions and group by and without having.
 
